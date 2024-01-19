61284a0809b85185e88e74466936ece0da7bc130
amitdahan64 <amit.dahan01@post.runi.ac.il>
2024-01-19 18:18:18 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -8,12 +8,12 @@
 public class GameOfLife {
 
 	public static void main(String[] args) {
-		String fileName = "square.dat";
+		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
 		///test1(fileName);
 		////test2(fileName);
-		test3(fileName, 3);
+		//test3(fileName, 3);
 		//play(fileName);
 	}
 	
